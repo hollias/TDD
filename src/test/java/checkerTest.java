faa2cbb0e54068e2ffddb2e7194a54b903e79140
hollias <clous.namsoo@gmail.com>
2019-05-01 23:04:59 +0900

    12장 클리어

--- src/test/java/checkerTest.java
+++ src/test/java/checkerTest.java
@@ -24,4 +24,15 @@ public class checkerTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
+    @Test
+    public void testSimpleAddition(){
+//        Money sum = Money.dollar(5).plus(Money.dollar(5));
+//        assertEquals(Money.dollar(10), sum);
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "CHF");
+        assertEquals(Money.dollar(10), reduced);
+
+    }
 }
